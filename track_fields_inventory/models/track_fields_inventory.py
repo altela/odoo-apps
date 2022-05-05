a27358c9eaccf166bf220fce8665d68681e9dd3d
Altela Eleviansyah Pramardhika <altela_pramardhika@yahoo.com>
2022-05-05 12:04:37 +0800

    Added tracking on fields inside General Information : - type - categ_id - default_code - list_price - standard_price - uom_id - uom_po_id - description

--- track_fields_inventory/models/track_fields_inventory.py
+++ track_fields_inventory/models/track_fields_inventory.py
@@ -7,5 +7,18 @@ class TrackFieldsInventory(models.Model):
     name = fields.Char(string='Product Name', track_visibility='always')
     sale_ok = fields.Boolean(string='Can be Sold', track_visibility='always')
     purchase_ok = fields.Boolean(string='Can be Purchased', track_visibility='always')
+
+    # General Information
+    type = fields.Selection(string='Product Type', track_visibility='always')
+    categ_id = fields.Many2one(string='Product Category', track_visibility='always')
+    default_code = fields.Char(string='Internal Reference', track_visibility='always')
     barcode = fields.Char(string='Barcode', track_visibility='always')
+    list_price = fields.Float(string='Sales Price', track_visibility='always')
+    standard_price = fields.Float(string='Cost', track_visibility='always')
+    uom_id = fields.Many2one(string='Unit of Measure', track_visibility='always')
+    uom_po_id = fields.Many2one(string='Unit of Measure', track_visibility='always')
+    description = fields.Text(string='Description', track_visibility='always')
+
+
+
 
