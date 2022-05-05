f782f09a27e7e77958cdd224a173668d42048155
Altela Eleviansyah Pramardhika <altela_pramardhika@yahoo.com>
2022-05-05 10:20:03 +0800

    Added tracking on fields : - barcode - purchase_ok - sale_ok

--- track_fields_inventory/models/track_fields_inventory.py
+++ track_fields_inventory/models/track_fields_inventory.py
@@ -5,3 +5,7 @@ class TrackFieldsInventory(models.Model):
     _inherit = "product.template"
 
     name = fields.Char(string='Product Name', track_visibility='always')
+    sale_ok = fields.Boolean(string='Can be Sold', track_visibility='always')
+    purchase_ok = fields.Boolean(string='Can be Purchased', track_visibility='always')
+    barcode = fields.Char(string='Barcode', track_visibility='always')
+
