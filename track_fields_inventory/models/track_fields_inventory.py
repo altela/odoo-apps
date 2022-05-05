066afb45f6c95c8a40a64a3ed23a9d2160beef64
Altela Eleviansyah Pramardhika <altela_pramardhika@yahoo.com>
2022-05-05 14:49:02 +0800

    Added Sales, Vendor, and Whole Inventory Tab

--- track_fields_inventory/models/track_fields_inventory.py
+++ track_fields_inventory/models/track_fields_inventory.py
@@ -8,6 +8,9 @@ class TrackFieldsInventory(models.Model):
     sale_ok = fields.Boolean(string='Can be Sold', track_visibility='always')
     purchase_ok = fields.Boolean(string='Can be Purchased', track_visibility='always')
 
+    # Smart Button
+    active = fields.Boolean(string='Active', track_visibility='always')
+
     # General Information
     type = fields.Selection(string='Product Type', track_visibility='always')
     categ_id = fields.Many2one(string='Product Category', track_visibility='always')
@@ -19,6 +22,25 @@ class TrackFieldsInventory(models.Model):
     uom_po_id = fields.Many2one(string='Unit of Measure', track_visibility='always')
     description = fields.Text(string='Description', track_visibility='always')
 
+    # Sales
+    description_sale = fields.Text(string='Sale Description', track_visibility='always')
 
+    # Vendor
+    description_purchase = fields.Text(string='Description for Vendors', track_visibility='always')
 
-
+    # Inventory
+    route_ids = fields.Many2many(string='Routes', track_visibility='always')
+    sale_delay = fields.Float(string='Customer Lead Time', track_visibility='always')
+    weight = fields.Float(string='Weight', track_visibility='always')
+    volume = fields.Float(string='Volume', track_visibility='always')
+    responsible_id = fields.Many2one(string='Responsible', track_visibility='always')
+    property_stock_production = fields.Many2one(string='Production Location', track_visibility='always')
+    property_stock_inventory = fields.Many2one(string='Inventory Location', track_visibility='always')
+    description_pickingout = fields.Text(string='Description on Delivery Orders', track_visibility='always')
+    description_pickingin = fields.Text(string='Description for Receipts', track_visibility='always')
+    description_picking = fields.Text(string='Description for Internal Transfers', track_visibility='always')
+    tracking = fields.Selection(string='Tracking', track_visibility='always')
+    use_time = fields.Integer(string='Product Use Time', track_visibility='always')
+    life_time = fields.Integer(string='Product Life Time', track_visibility='always')
+    removal_time = fields.Integer(string='Product Removal Time', track_visibility='always')
+    alert_time = fields.Integer(string='Product Alert Time', track_visibility='always')
