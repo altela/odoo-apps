fbc17b9045c7a3a5f66419547f3c521d6729d5aa
Altela Eleviansyah Pramardhika <altela_pramardhika@yahoo.com>
2022-05-13 14:26:00 +0800

    Added track_fields_inventory

--- track_fields_inventory/__manifest__.py
+++ track_fields_inventory/__manifest__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
-    'name': 'Edit History of Inventory',
+    'name': 'Show Edit History of Inventory',
     'author': 'Altela Eleviansyah Pramardhika',
-    'version': '12.0.1.0.0',
+    'version': '14.0.1.0.0',
     'summary': 'Create history into chatter after editing inventory',
     'license': 'LGPL-3',
     'sequence': 1,
--- track_fields_inventory/models/track_fields_inventory.py
+++ track_fields_inventory/models/track_fields_inventory.py
@@ -1,4 +1,5 @@
-from odoo import models, fields, api
+from odoo import models, fields
+from odoo.addons.base.models.res_partner import WARNING_MESSAGE, WARNING_HELP
 
 
 class TrackFieldsInventory(models.Model):
@@ -26,12 +27,25 @@ class TrackFieldsInventory(models.Model):
     description_sale = fields.Text(string='Sale Description', track_visibility='always')
     available_in_pos = fields.Boolean(string='Available in PoS', track_visibility='always')
     to_weight = fields.Boolean(string='To Weigh With Scale', track_visibility='always')
+    invoice_policy = fields.Selection([
+        ('order', 'Ordered quantities'),
+        ('delivery', 'Delivered quantities')], string='Invoicing Policy',
+        help='Ordered Quantity: Invoice quantities ordered by the customer.\n'
+             'Delivered Quantity: Invoice quantities delivered to the customer.',
+        default='delivery', track_visibility='always')
 
-    # Vendor
+    # Purchase
     description_purchase = fields.Text(string='Description for Vendors', track_visibility='always')
+    purchase_method = fields.Selection([
+        ('purchase', 'On ordered quantities'),
+        ('receive', 'On received quantities'),
+    ], string="Control Policy", default="receive", track_visibility='always')
+    service_to_purchase = fields.Boolean("Purchase Automatically", track_visibility='always')
+    purchase_line_warn = fields.Selection(WARNING_MESSAGE, 'Purchase Order Line', help=WARNING_HELP, required=True,
+                                          default="no-message", track_visibility='always')
+    purchase_line_warn_msg = fields.Text('Message for Purchase Order Line', track_visibility='always')
 
     # Inventory
-    route_ids = fields.Many2many(string='Routes', track_visibility='always')
     sale_delay = fields.Float(string='Customer Lead Time', track_visibility='always')
     weight = fields.Float(string='Weight', track_visibility='always')
     volume = fields.Float(string='Volume', track_visibility='always')
@@ -46,3 +60,15 @@ class TrackFieldsInventory(models.Model):
     life_time = fields.Integer(string='Product Life Time', track_visibility='always')
     removal_time = fields.Integer(string='Product Removal Time', track_visibility='always')
     alert_time = fields.Integer(string='Product Alert Time', track_visibility='always')
+    produce_delay = fields.Float('Manufacturing Lead Time', default=0.0, track_visibility='always')
+
+
+
+    # Accounting
+    property_account_income_id = fields.Many2one('account.account.template',
+                                                 string='Income Account', track_visibility='always')
+    property_account_expense_id = fields.Many2one('account.account.template',
+                                                  string='Expense Account', track_visibility='always')
+    property_account_creditor_price_difference = fields.Many2one(
+        'account.account', string="Price Difference Account", track_visibility='always')
+
