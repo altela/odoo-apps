15b566e42f124b5f7e07c25092f6ba5085d14950
Altela Eleviansyah Pramardhika <altela_pramardhika@yahoo.com>
2022-05-13 15:17:11 +0800

    Change module name

--- track_fields_inventory/__manifest__.py
+++ track_fields_inventory/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
-    'name': 'Edit History of Inventory',
+    'name': 'Show Edit History of Inventory',
     'author': 'Altela Eleviansyah Pramardhika',
     'version': '12.0.1.0.0',
     'summary': 'Create history into chatter after editing inventory',
