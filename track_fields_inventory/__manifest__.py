923343ca72a95aec88aa205a8631761f2713360b
Altela Eleviansyah Pramardhika <altela_pramardhika@yahoo.com>
2022-07-15 13:22:59 +0800

    Update Author

--- track_fields_inventory/__manifest__.py
+++ track_fields_inventory/__manifest__.py
@@ -2,7 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
     'name': 'Show Edit History of Inventory',
-    'author': 'Altela Eleviansyah Pramardhika',
+    'author': 'Altela Softwares',
     'version': '15.0.1.0.0',
     'summary': 'Create history into chatter after editing inventory',
     'license': 'LGPL-3',
