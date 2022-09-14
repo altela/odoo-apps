8f02505344c7d34d3c8d8e55e810f26d5687f1ea
Altela Eleviansyah Pramardhika <altela_pramardhika@yahoo.com>
2022-09-14 13:17:32 +0800

    Changed how the way barcode are pulled using related field

--- barcode_inside_purchase/models/barcode_inside_purchase.py
+++ barcode_inside_purchase/models/barcode_inside_purchase.py
@@ -4,10 +4,4 @@ from odoo import models, fields, api
 class BarcodeInsidePurchase(models.Model):
     _inherit = 'purchase.order.line'
 
-    # product_id = fields.Many2one('product.template', 'product_id')
-    barcode = fields.Text(string='Barcode')
-
-    # barcode = fields.Many2one('product.template', 'barcode')
-    @api.onchange('product_id')
-    def pull_barcode(self):
-        self.barcode = self.product_id.barcode
+    barcode = fields.Char('barcode', related='product_id.barcode')
