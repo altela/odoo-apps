1e500f3a22f47debaf0599dab55598ca2b4700f6
Altela Eleviansyah Pramardhika <altela_pramardhika@yahoo.com>
2023-02-01 22:36:02 +0800

    Bump to version 15.0.2.0.0

--- clear_order_lines/__manifest__.py
+++ clear_order_lines/__manifest__.py
@@ -3,7 +3,7 @@
 {
     'name': 'Clear Order Lines',
     'author': 'Central Jayatama Abadi',
-    'version': '15.0.1.0.0',
+    'version': '15.0.2.0.0',
     'summary': 'Clear Order Lines inside The Sales and Purchase Order',
     'license': 'LGPL-3',
     'sequence': 1,
