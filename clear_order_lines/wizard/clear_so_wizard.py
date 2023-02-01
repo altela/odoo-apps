a6ba784ed85de71bd03d3bd2da838a110941ff13
Altela Eleviansyah Pramardhika <altela_pramardhika@yahoo.com>
2023-02-01 22:46:30 +0800

    Bump to version 14.0.2.0.0 (#3)

--- clear_order_lines/wizard/clear_so_wizard.py
+++ clear_order_lines/wizard/clear_so_wizard.py
@@ -2,13 +2,14 @@ from odoo import api, models, fields
 
 class ClearSoWizard(models.TransientModel):
     _name = "clear.order.lines.so"
+    _description = "Clear Order Lines Button"
 
     order_line = fields.One2many('sale.order.line', 'order_id')
 
     def clear_order_lines(self):
-        records = self.env['sale.order'].browse(self._context.get('active_ids', []))
-        for record in records:
+        # records = self.env['sale.order'].browse(self._context.get('active_ids', []))
+        for record in self.purchase_order_lines:
             record.order_line = [(5, 0, 0)]
 
         msg_body = 'Has cleared order lines'
-        records.message_post(body=msg_body, message_type='notification')
+        self.message_post(body=msg_body, message_type='notification')
