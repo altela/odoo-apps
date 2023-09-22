cf66409ce37423daddce443a504abd1acafd9fd9
Altela Eleviansyah Pramardhika <altela_pramardhika@yahoo.com>
2023-09-22 10:05:19 +0800

    [FIX] Change author and website

--- barcode_inside_purchase/__manifest__.py
+++ barcode_inside_purchase/__manifest__.py
@@ -2,14 +2,14 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
     'name': 'Barcode Inside Purchase',
-    'author': 'Altela Softwares',
+    'author': 'Altela Software',
     'version': '12.0.1.0.0',
     'summary': 'Added barcode column inside purchase order',
     'license': 'LGPL-3',
     'sequence': 1,
     'description': """Create history into chatter after editing inventory""",
     'category': 'Extra Tools',
-    'website': 'https://www.altela.net',
+    'website': 'https://www.altelasoftware.com',
     'depends': [
         'purchase',
     ],
